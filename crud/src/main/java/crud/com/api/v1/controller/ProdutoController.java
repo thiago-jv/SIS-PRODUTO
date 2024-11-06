cc8824c191c15fc7ee6913b1986167071b584d1d
Thiago Henrique <thiago.henrique.25@hotmail.com>
2024-11-06 06:55:03 -0500

    chore: Adicionando logs na camada dos controllers

--- crud/src/main/java/crud/com/api/v1/controller/ProdutoController.java
+++ crud/src/main/java/crud/com/api/v1/controller/ProdutoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(value = "/v1/produtos", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -24,29 +25,36 @@ public class ProdutoController {
     @Autowired
     private ProdutoMapper produtoMapper;
 
+    private static Logger log = Logger.getLogger(ProdutoController.class.getName());
+
     @PostMapping
     public ProdutoResponseDTO criar(@Valid @RequestBody ProdutoPostDTO produtoPostDTO, HttpServletResponse response) {
+        log.info("Cadastrando Produto");
         return produtoMapper.toProdutoResponseDTO(produtoService.criar(produtoMapper.toProduto(produtoPostDTO)));
     }
 
     @PutMapping("/{id}")
     public ProdutoResponseDTO atualizar(@PathVariable Long id, @Valid @RequestBody ProdutoPutDTO produtoPutDTO) {
+        log.info("Atualizando Produto");
         return produtoMapper.toProdutoResponseDTO(produtoService.atualizar(id, produtoMapper.toProduto(produtoPutDTO)));
     }
 
     @GetMapping
     public List<ProdutoResponseDTO> listar() {
+        log.info("Listando Produto");
         return produtoMapper.toListProdutoResponseDTO(produtoService.listar());
     }
 
     @GetMapping("/{id}")
     public ProdutoResponseDTO buscarPorId(@PathVariable Long id) {
+        log.info("Busca Produto por ID");
         return produtoMapper.toProdutoResponseDTO(produtoService.buscarOuFalhar(id));
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar(@PathVariable Long id) {
+        log.info("Deleta Produto por ID");
         produtoService.deletar(id);
     }
 
